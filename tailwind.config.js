f50697c9375178d5ba5055209f840a161df36870
Abigail <ashbella333@gmail.com>
2023-01-19 09:56:38 +0000

    added font family to extend object

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Poppins']
+      }
+    },
   },
   plugins: [],
 }
